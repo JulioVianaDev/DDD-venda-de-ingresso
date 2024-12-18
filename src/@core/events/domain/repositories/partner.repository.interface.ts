42dab05735af53c964be167f209badc394161292
juliovianadev <juliovianadev@gmail.com>
2024-12-18 00:25:44 -0300

    criando a primeira implementação de um repositório

--- src/@core/events/domain/repositories/partner.repository.interface.ts
+++ src/@core/events/domain/repositories/partner.repository.interface.ts
@@ -1,4 +1,5 @@
-import { IRepository } from '../../../common/domain/repository-interface';
+import { IRepository } from 'src/@core/common/domain/repositories/repository-interface';
 import { Partner } from '../entities/partner.entity';
+
 // eslint-disable-next-line @typescript-eslint/no-empty-interface
-export interface PartnerRepositoryInterface extends IRepository<Partner> { }
+export interface IPartnerRepository extends IRepository<Partner> { }
